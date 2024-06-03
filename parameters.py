d357c49328d1252d7d6e7415169866538808f807
VicenteLavagnino <vicente.lavagnino@uc.cl>
2024-06-02 20:47:26 -0400

    works :)

--- parameters.py
+++ parameters.py
@@ -1,12 +1,24 @@
 import pandas as pd
 
 # Leer datos desde archivos CSV
-e_profesor_profesor = pd.read_csv('source/seeds/E-profesor-profesor.csv', header=0, index_col=0)
-j_profesor_bloque = pd.read_csv('source/seeds/J-profesor-bloque.csv', header=0, index_col=0)
-postulaciones_profesores_ramos = pd.read_csv('source/seeds/postulaciones_profesores_ramos.csv', header=0, index_col=0)
-preferencias_profesores_bloques = pd.read_csv('source/seeds/preferencias_profesores_bloques.csv', header=0, index_col=0)
-puede_manejar_profesores = pd.read_csv('source/seeds/puede_manejar_profesores.csv', header=0, index_col=0)
-q_profesor_ramo = pd.read_csv('source/seeds/Q-profesor-ramo.csv', header=0, index_col=0)
+#e_profesor_profesor = pd.read_csv('source/seeds/E-profesor-profesor.csv', header=0, index_col=0)
+e_profesor_profesor = pd.read_csv('source/distancias_entre_profes.csv', header=None)
+
+#j_profesor_bloque = pd.read_csv('source/seeds/J-profesor-bloque.csv', header=0, index_col=0)
+j_profesor_bloque = pd.read_csv('source/distancias_profe_colegio.csv', header=None)
+
+#postulaciones_profesores_ramos = pd.read_csv('source/seeds/postulaciones_profesores_ramos.csv', header=0, index_col=0)
+postulaciones_profesores_ramos = pd.read_csv('source/ramos_postulados.csv', header=None)
+
+#preferencias_profesores_bloques = pd.read_csv('source/seeds/preferencias_profesores_bloques.csv', header=0, index_col=0)
+preferencias_profesores_bloques = pd.read_csv('source/preferencias_bloque.csv', header=None)
+
+#puede_manejar_profesores = pd.read_csv('source/seeds/puede_manejar_profesores.csv', header=0, index_col=0)
+puede_manejar_profesores = pd.read_csv('source/maneja.csv', header=None)
+
+#q_profesor_ramo = pd.read_csv('source/seeds/Q-profesor-ramo.csv', header=0, index_col=0)
+q_profesor_ramo = pd.read_csv('source/requisitos_bloque.csv', header=None)
+
 tiene_preferencia_profesores = pd.read_csv('source/seeds/tiene_preferencia_profesores.csv', header=0, index_col=0)
 
 # Verificar que no haya valores NaN en los datos de entrada
@@ -43,8 +55,8 @@ I = [0, 1, 2, 3, 4]
 # Crear parámetros
 Ep1_p2 = {(i, j): int(e_profesor_profesor.iat[i, j]) for i in P for j in P}
 Jp_b = {(i, j): int(j_profesor_bloque.iat[i, j]) for i in P for j in B}
-#Dp_r = {(i, j): int(postulaciones_profesores_ramos.iat[i, j]) for i in P for j in R}
-Dp_r = {(i, j): 1 for i in P for j in R}
+Dp_r = {(i, j): int(postulaciones_profesores_ramos.iat[i, j]) for i in P for j in R}
+#Dp_r = {(i, j): 1 for i in P for j in R}
 
 #Fp_b = {(i, j): int(preferencias_profesores_bloques.iat[i, j]) for i in P for j in B}
 Fp_b = {(i, j): 1 for i in P for j in B}
